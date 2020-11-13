d5534f9375387435f710f3ef9b87c5b8474a99fc
damien <devoti.damien@gmail.com>
2020-11-13 22:46:01 +0100

    Add first entity and controller for easy admin

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
@@ -24,6 +25,7 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex(pattern="/^[a-z0-9\-]+$/")
      */
     private $slug;
 
